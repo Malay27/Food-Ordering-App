6dc294f502fd9018e78134c246dec1276cae20dc
malay <malayjiyani1987@gmail.com>
2023-11-01 13:15:41 +0530

    Uploaded

--- App.js
+++ App.js
@@ -1,9 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+const title = ()=>(
+    <h1 id="title">Namaste React</h1>
+);
 
-const heading = <h1 id="h1">This is my Heading</h1>
+const HeadingComponents = () => {
+  return (
+    <div>
+      {title}
+      <h2>Namaste React Functional Components</h2>
+      <h2>This is a h2 tag</h2>
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(container);
+root.render(<HeadingComponents />);
